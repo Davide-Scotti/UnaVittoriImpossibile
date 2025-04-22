671a55af0c108ae95b7b0508d97496053a8723de
dscotti <dscotti@seilaser.com>
2025-04-22 15:53:07 +0200

    Aggiunto inizialize del gioco e lancio dei dadi

--- src/Board.h
+++ src/Board.h
@@ -2,23 +2,38 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+#include "RisorseGlobali.h"
 #include "Zone.h"
-#include <vector>
 #include <string>
+#include <vector>
 
 class Board {
 private:
-    std::vector<Zone> zones;
+  std::vector<Zone> zones;
+  RisorseGlobali risorseGlobali;
 
 public:
-    // Costruttore: carica la configurazione di default
-    Board();
+  // Costruttore: carica la configurazione di default
+  Board();
 
-    // Ritorna riferimento alla zona (per controlli avanzati)
-    Zone& getZone(const int& nZone);
+  // Ritorna riferimento alla zona (per controlli avanzati)
+  Zone &getZone(const int &nZone);
 
-    // Stampa su console tutte le zone con i loro livelli
-    void display() const;
+  // Aggiunte
+  bool aggiungiFascistaAZona(int numeroZona);
+  bool aggiungiBarricataAZona(int numeroZona);
+  bool aggiungiMunizioneAZona(int numeroZona);
+  bool aggiungiPozioneAZona(int numeroZona);
+  bool aggiungiCivileAZona(int numeroZona);
+
+  // Rimozioni
+  bool rimuoviFascistaDaZona(int numeroZona);
+  bool rimuoviMunizioneDaZona(int numeroZona);
+  bool rimuoviPozioneDaZona(int numeroZona);
+  bool rimuoviCivileDaZona(int numeroZona);
+
+  // Stampa su console tutte le zone con i loro livelli
+  void display() const;
 };
 
 #endif // BOARD_H
